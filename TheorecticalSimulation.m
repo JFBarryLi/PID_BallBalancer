5c45a2ea4e67269680340625bf5957e6d9a62da4
Barry Li <bli031@uottawa.ca>
2018-12-28 01:23:10 -0500

    Added units

--- TheorecticalSimulation.m
+++ TheorecticalSimulation.m
@@ -1,22 +1,22 @@
 %Ball Balancer PID Thoerectical Simulation
 
-%Mass of ball
+%Mass of ball [kg]
 m = 0.00246;
-%Radius of ball
+%Radius of ball [m]
 R = 0.03175;
-%Gravity
+%Gravity [m/s^2]
 g = -9.81;
-%Length of beam
+%Length of beam [m]
 L = 0.30; 
-%Lever arm length
+%Lever arm length [m]
 d = 0.1438;
-%Moment of inertia of the ball
+%Polar Moment of inertia of the ball [m^4]
 J = 3.086e-6;
 
 %Transfer function
 s = tf('s');
 
-%Position on beam
+%Position on beam [m]
 Pos = -m*g*d/L/(J/R^2+m)/s^2;
 
 %Proportional constant
